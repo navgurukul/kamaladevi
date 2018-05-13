2135ba51bb6ab7b36948946c5bf10ef050b49ce7
Pushkar Sharma <thepsguy@icloud.com>
2018-05-14 02:43:20 +0530

    google auth sign in

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -5,6 +5,7 @@ import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
+import GoogleLogin from 'react-google-login';
 
 
 const styles = theme => ({
@@ -18,25 +19,51 @@ const styles = theme => ({
 	},
 });
 
+const responseGoogle = (response) => {
+  console.log(response);
+}
 
 const LandingPage = (props) => {
 	const { classes } = props;
 	return (
 		<div className={classes.root}>
-			<Paper style={{ width: 'auto', padding: 20, margin: 20 }}>
+			<Paper style={{width: '25%', padding: 20, margin: 'auto' }}>
 				<Typography
 					variant="headline"
 					gutterBottom
 				>
-            Idhar ya to login rahega, ya list courses, depending on
-            authentication status!
-				</Typography>
-				<br />
-				<Link prefetch href="/home" passHref>
-					<Button variant="raised" color="secondary">
-            List Courses
-					</Button>
-				</Link>
+          SARAL <br/>
+        </Typography>
+          <Typography
+            variant="caption"
+            gutterBottom
+          >
+          NavGurukul eLearning Platform
+        </Typography>
+        <br />
+          <GoogleLogin
+            clientId="330505979484-sgfkanh7p0nsqvua8susd9q60i94dnbh.apps.googleusercontent.com"
+            buttonText="Sign In"
+            cookiePolicy='single_host_origin'
+            scope='profile email'
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+          />
+        <br/><br/>
+        <Typography
+          variant="body1"
+          gutterBottom
+        >
+          Never doubt that a small group of thoughtful, committed citizens can change the world;
+          indeed, it's the only thing
+          that ever has.
+          <p style={{textAlign: 'right'}}>~ Margaret Mead</p>
+        </Typography>
+				{/*<Link prefetch href="/home" passHref>*/}
+					{/*<Button variant="raised" color="secondary">*/}
+            {/*List Courses*/}
+					{/*</Button>*/}
+				{/*</Link>*/}
 			</Paper>
 		</div>);
 };
