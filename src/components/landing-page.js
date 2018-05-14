80e552efbeaf21fa00c51a9a64fb66c6e5ce8c0d
Pushkar Sharma <thepsguy@icloud.com>
2018-05-14 19:48:02 +0530

    switched to tabs

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -20,50 +20,50 @@ const styles = theme => ({
 });
 
 const responseGoogle = (response) => {
-  console.log(response);
-}
+	console.log(response);
+};
 
 const LandingPage = (props) => {
 	const { classes } = props;
 	return (
 		<div className={classes.root}>
-			<Paper style={{width: '25%', padding: 20, margin: 'auto' }}>
+			<Paper style={{ width: '25%', padding: 20, margin: 'auto' }}>
 				<Typography
 					variant="headline"
 					gutterBottom
 				>
-          SARAL <br/>
-        </Typography>
-          <Typography
-            variant="caption"
-            gutterBottom
-          >
-          NavGurukul eLearning Platform
-        </Typography>
-        <br />
-          <GoogleLogin
-            clientId="330505979484-sgfkanh7p0nsqvua8susd9q60i94dnbh.apps.googleusercontent.com"
-            buttonText="Sign In"
-            cookiePolicy='single_host_origin'
-            scope='profile email'
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-          />
-        <br/><br/>
-        <Typography
-          variant="body1"
-          gutterBottom
-        >
-          Never doubt that a small group of thoughtful, committed citizens can change the world;
-          indeed, it's the only thing
-          that ever has.
-          <p style={{textAlign: 'right'}}>~ Margaret Mead</p>
-        </Typography>
-				{/*<Link prefetch href="/home" passHref>*/}
-					{/*<Button variant="raised" color="secondary">*/}
-            {/*List Courses*/}
-					{/*</Button>*/}
-				{/*</Link>*/}
+					SARAL <br />
+				</Typography>
+				<Typography
+					variant="caption"
+					gutterBottom
+				>
+					NavGurukul eLearning Platform
+				</Typography>
+				<br />
+				<GoogleLogin
+					clientId="330505979484-sgfkanh7p0nsqvua8susd9q60i94dnbh.apps.googleusercontent.com"
+					buttonText="Sign In"
+					cookiePolicy="single_host_origin"
+					scope="profile email"
+					onSuccess={responseGoogle}
+					onFailure={responseGoogle}
+				/>
+				<br /><br />
+				<Typography
+					variant="body1"
+					gutterBottom
+				>
+					Never doubt that a small group of thoughtful, committed citizens can change the world;
+					indeed, it's the only thing
+					that ever has.
+					<p style={{ textAlign: 'right' }}>~ Margaret Mead</p>
+				</Typography>
+				{/* <Link prefetch href="/home" passHref> */}
+				{/* <Button variant="raised" color="secondary"> */}
+				{/* List Courses */}
+				{/* </Button> */}
+				{/* </Link> */}
 			</Paper>
 		</div>);
 };
