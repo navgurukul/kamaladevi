ee288696b61531705b5aa8d775985e5547ce55ce
Abhishek <abhishek@credifiable.com>
2018-05-15 10:24:15 +0530

    material-ui upgraded to @material-ui/core

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'material-ui/Button';
-import Paper from 'material-ui/Paper';
-import Typography from 'material-ui/Typography';
-import { withStyles } from 'material-ui/styles';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import GoogleLogin from 'react-google-login';
 
 
