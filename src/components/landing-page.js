668e0fa4ead97d63a3234c1733b0c1da73074bcc
Abhishek <abhishek@credifiable.com>
2018-05-15 10:10:19 +0530

    Implemented suggestinos from pull/2

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
@@ -14,8 +13,9 @@ const styles = theme => ({
 		paddingTop: theme.spacing.unit * 20,
 	},
 	paper: {
-		paddingTop: theme.spacing.unit * 2,
-		paddingBottom: theme.spacing.unit * 2,
+		width: '25%',
+		margin: 'auto',
+		padding: theme.spacing.unit * 2.5,
 	},
 });
 
@@ -27,7 +27,7 @@ const LandingPage = (props) => {
 	const { classes } = props;
 	return (
 		<div className={classes.root}>
-			<Paper style={{ width: '25%', padding: 20, margin: 'auto' }}>
+			<Paper className={classes.paper}>
 				<Typography
 					variant="headline"
 					gutterBottom
@@ -41,29 +41,34 @@ const LandingPage = (props) => {
 					NavGurukul eLearning Platform
 				</Typography>
 				<br />
-				<GoogleLogin
+				<Button
+					color="primary"
+					variant="raised"
+					component={GoogleLogin}
 					clientId="330505979484-sgfkanh7p0nsqvua8susd9q60i94dnbh.apps.googleusercontent.com"
-					buttonText="Sign In"
 					cookiePolicy="single_host_origin"
 					scope="profile email"
 					onSuccess={responseGoogle}
 					onFailure={responseGoogle}
-				/>
+				>
+          Sign In
+				</Button>
 				<br /><br />
 				<Typography
 					variant="body1"
 					gutterBottom
 				>
 					Never doubt that a small group of thoughtful, committed citizens can change the world;
-					indeed, it's the only thing
+					indeed, it&apos;s the only thing
 					that ever has.
-					<p style={{ textAlign: 'right' }}>~ Margaret Mead</p>
 				</Typography>
-				{/* <Link prefetch href="/home" passHref> */}
-				{/* <Button variant="raised" color="secondary"> */}
-				{/* List Courses */}
-				{/* </Button> */}
-				{/* </Link> */}
+				<Typography
+					variant="body1"
+					gutterBottom
+					style={{ textAlign: 'right' }}
+				>
+          ~ Margaret Mead
+				</Typography>
 			</Paper>
 		</div>);
 };
