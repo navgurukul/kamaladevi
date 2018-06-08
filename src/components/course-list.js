9c783c1aaebcfe7b3d6887898dc63164d6512906
Praharsh92 <praharsh@credifiable.com>
2018-06-08 21:25:22 +0530

    Grid Implementation in course list

--- src/components/course-list.js
+++ src/components/course-list.js
@@ -7,26 +7,66 @@ import localforage from 'localforage';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
 
 import { fetchApi } from '../services/api';
 
 const styles = theme => ({
+	rootLoader: {
+		display: 'flex',
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 	root: {
-		textAlign: 'center',
-		paddingTop: theme.spacing.unit * 20,
+		display: 'flex',
+		justifyContent: 'center',
+		paddingBottom: 50,
 	},
-	button: {
+	rootContent: {
+		paddingTop: theme.spacing.unit * 5,
+		display: 'flex',
+		flexDirection: 'column',
+		width: '80%',
+		[theme.breakpoints.down('sm')]: {
+			width: '100%',
+			paddingLeft: 10,
+			paddingRight: 10,
+		},
 	},
-	card: {
-		backgroundColor: 'white',
+	cardMarginRightBot: {
+		marginRight: 25,
+		height: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		[theme.breakpoints.down('sm')]: {
+			marginRight: 2.5,
+		},
+	},
+	cardMarginLeftBot: {
+		marginLeft: 25,
+		height: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		[theme.breakpoints.down('sm')]: {
+			marginRight: 2.5,
+		},
+	},
+	cardGrid: {
+		marginBottom: 10,
+	},
+	cardAction: {
+		justifyContent: 'flex-end',
 	},
 });
-
+// change rootContent, cardMarginRightBot, cardMarginLeftBot, cardGrid different spacing
 
 class CourseList extends React.Component {
 	constructor(props) {
@@ -69,110 +109,128 @@ class CourseList extends React.Component {
 		} = this.state;
 		if (!prefetchedData) {
 			return (
-				<div className={classes.root}>
+				<div className={classes.rootLoader}>
 					<CircularProgress className={classes.progress} size={50} />
 				</div>);
 		}
 		return (
 			<div className={classes.root}>
-				{enrolledCourses != [] ? (
-					<h2>Aapke Courses</h2>
-				) : ''}
-				{enrolledCourses.map(value => (
-					<div key={value.id}>
-						<Card variant="raised" className={classes.card}>
-							<CardContent>
-								<Typography variant="headline" component="h2">
-									{value.name}
+				<div className={classes.rootContent}>
+					<Grid container spacing={0}>
+						<Grid	item xs={12} >
+							{enrolledCourses !== [] ? (
+								<Typography variant="headline" component="h2" align="center" gutterBottom>
+								Aapke Courses
 								</Typography>
-								<Typography color="textSecondary">
-									{value.shortDescription}
-								</Typography>
-								<Typography component="p">
-									<LinearProgress variant="determinate" value={value.daysSinceEnrolled * 100 / value.daysToComplete} />
-								</Typography>
-							</CardContent>
-							<CardActions aligh="end">
-								<Button
-									size="small"
-									variant="raised"
-									color="primary"
-									className={classes.button}
-									onClick={() => Router.push({
-										pathname: '/course',
-										query: { id: value.id },
-									})}
-								>
+							) : ''}
+						</Grid>
+						{enrolledCourses.map(value => (
+							<Grid	item xs={6} key={value.id}>
+								<Card variant="raised" className={classes.card}>
+									<CardContent>
+										<Typography variant="headline" component="h2">
+											{value.name}
+										</Typography>
+										<Typography color="textSecondary">
+											{value.shortDescription}
+										</Typography>
+										<Typography component="p">
+											<LinearProgress variant="determinate" value={(value.daysSinceEnrolled * 100) / value.daysToComplete} />
+										</Typography>
+									</CardContent>
+									<CardActions aligh="end">
+										<Button
+											size="small"
+											variant="raised"
+											color="primary"
+											className={classes.button}
+											onClick={() => Router.push({
+												pathname: '/course',
+												query: { id: value.id },
+											})}
+										>
                   Preview
-								</Button>
-							</CardActions>
-						</Card>
-					</div>
-				))}
-
-				{availableCourses != [] ? (
-					<h2>Aap yeh courses mein enroll kar skte hai</h2>
-				) : ''}
-				{availableCourses.map(value => (
-					<div key={value.id} >
-						<Card variant="raised" className={classes.card}>
-							<CardContent>
-								<Typography variant="headline" component="h2">
-									{value.name}
+										</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
+					</Grid>
+					<Grid container spacing={0}>
+						<Grid	item xs={12} >
+							{availableCourses !== [] ? (
+								<Typography variant="headline" component="h2" align="left" gutterBottom>
+								Aap yeh courses mein enroll kar skte hai
 								</Typography>
-								<Typography color="textSecondary">
-									{value.shortDescription}
-								</Typography>
-							</CardContent>
-							<CardActions aligh="end">
-								<Button
-									size="small"
+							) : ''}
+						</Grid>
+						{availableCourses.map((value, key) => (
+							<Grid	item xs={6} key={value.id} className={classes.cardGrid}>
+								<Card
 									variant="raised"
-									color="primary"
-									className={classes.button}
-									onClick={() => Router.push({
-										pathname: '/course',
-										query: { id: value.id },
-									})}
+									className={
+										(key % 2 === 0)
+											? classes.cardMarginRightBot
+											: classes.cardMarginLeftBot}
 								>
+									<CardContent>
+										<Typography variant="headline" component="h2">
+											{value.name}
+										</Typography>
+										<Typography color="textSecondary">
+											{value.shortDescription}
+										</Typography>
+									</CardContent>
+									<CardActions aligh="end" className={classes.cardAction}>
+										<Button
+											size="small"
+											variant="raised"
+											color="primary"
+											onClick={() => Router.push({
+												pathname: '/course',
+												query: { id: value.id },
+											})}
+										>
                   Preview
-								</Button>
-							</CardActions>
-						</Card>
-					</div>
-				))}
+										</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
 
-				{facilitatingCourses == [] ? (
-					<h2>Aap inn courses ko facilitate kar rahe hain</h2>
-				) : facilitatingCourses.size}
-				{facilitatingCourses.map(value => (
-					<div key={value.id} >
-						<Card variant="raised" className={classes.card}>
-							<CardContent>
-								<Typography variant="headline" component="h2">
-									{value.name}
-								</Typography>
-								<Typography color="textSecondary">
-									{value.shortDescription}
-								</Typography>
-							</CardContent>
-							{/* <CardActions aligh="end"> */}
-							{/* <Button */}
-							{/* size="small" */}
-							{/* variant="raised" */}
-							{/* color="primary" */}
-							{/* className={classes.button} */}
-							{/* onClick={() => Router.push({ */}
-							{/* pathname: '/course', */}
-							{/* query: { id: value.id }, */}
-							{/* })} */}
-							{/* > */}
-							{/* Preview */}
-							{/* </Button> */}
-							{/* </CardActions> */}
-						</Card>
-					</div>
-				))}
+						{facilitatingCourses === [] ? (
+							<h2>Aap inn courses ko facilitate kar rahe hain</h2>
+						) : facilitatingCourses.size}
+						{facilitatingCourses.map(value => (
+							<div key={value.id} >
+								<Card variant="raised" className={classes.card}>
+									<CardContent>
+										<Typography variant="headline" component="h2">
+											{value.name}
+										</Typography>
+										<Typography color="textSecondary">
+											{value.shortDescription}
+										</Typography>
+									</CardContent>
+									{/* <CardActions aligh="end"> */}
+									{/* <Button */}
+									{/* size="small" */}
+									{/* variant="raised" */}
+									{/* color="primary" */}
+									{/* className={classes.button} */}
+									{/* onClick={() => Router.push({ */}
+									{/* pathname: '/course', */}
+									{/* query: { id: value.id }, */}
+									{/* })} */}
+									{/* > */}
+									{/* Preview */}
+									{/* </Button> */}
+									{/* </CardActions> */}
+								</Card>
+							</div>
+						))}
+					</Grid>
+				</div>
 			</div>
 		);
 	}
