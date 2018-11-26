ad51587fb4b3b68c033a061d94401779a2ee0aa0
Amar Kumar Sinha <amar17@navgurukul.org>
2018-11-26 18:15:29 +0530

    change

--- src/components/peer-review/Peer-Review.js
+++ src/components/peer-review/Peer-Review.js
@@ -7,8 +7,8 @@ import Drawer from "@material-ui/core/Drawer";
 import IconButton from "@material-ui/core/IconButton";
 import Hidden from "@material-ui/core/Hidden";
 import MenuIcon from "@material-ui/icons/Menu";
-import assigmentsReviewSidenav from "./assigments-review-sidenav";
-import assigmentsReviewDetails from "./assigments-review-details";
+import PeerReviewSidenav from "./peer-review-sidenav";
+import PeerReviewDetails from "./peer-review-details";
 
 const drawerWidth = 240;
 
@@ -32,19 +32,13 @@ const styles = theme => ({
   }
 });
 
-class assigmentsReview extends React.Component {
+class Peereview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      assigments: [],
-      mobileOpen: false,
-      selectedCard:{}
+      peer: [],
+      mobileOpen: false
     };
-  }
-  updatedSelectedCard = (selectedCardDetail) => {
-    this.setState({
-      selectedCard:selectedCardDetail
-    })
   }
 
   handleDrawerToggle = () => {
@@ -66,54 +60,17 @@ class assigmentsReview extends React.Component {
     const { jwt } = value;
     try {
       response = await fetchApi(
-        `/assignments/assigmentsReview`,
+        `/assignments/peerReview`,
         {},
         { Authorization: jwt }
       );
-      const assigmentsReview = await response.data.data;
-      console.log(assigmentsReview);
-      this.setState({ 
-        assigments: assigmentsReview,
-        selectedCard: assigmentsReview[0],
-      });
+      const data = await response.data.data;
+      this.setState({ peer: data });
+      console.log(data);
     } catch (e) {
       console.log(e);
     }
-  } 
-  
-  // currentAssignment delete hona chaiye jab bi user Approve yeah Reject kare uske baad
-  // agla assignment show hona chaiye agar ha toh warna 
-  // Apke sare review ke kaam katam hogye display hona chaiye
-
-  // humne yeh function kyu banaya hai aur ishe tumhe kya lagta ha kidar istamal karna chaiye?
-  // aur kab?
-  // function remove(cards, index){
-  //   const newCards = [ ];
-  //   for(let i =0; i<cards.length;i++){
-  //      if(i===index){
-  //         continue;
-  //      }
-  //      newCards.push(cards[i]);
-  //   }
-  //   return newCards;
-  // }
-  removeCard = (assignments,index) => {
-    const {assignments:assigments,selectedCard:index} = this.state;
-    const newAssigmentsList = []
-    for (let i = 0; i<assignments.length;i++){
-      if (i === selectedCard) {
-        continue
-      }
-      newAssigmentsList.push(assignments[i])
-    }
-    return newAssigmentsList
-    this.setState = (
-      {
-        
-      }
-    )
   }
-  
   render() {
     const { classes } = this.props;
 
@@ -134,10 +91,7 @@ class assigmentsReview extends React.Component {
               paper: classes.drawerPaper
             }}
           >
-            <assigmentsReviewSidenav 
-              assigments={this.state.assigments} 
-              updatedSelectedCard={this.updatedSelectedCard} 
-            />
+            <PeerReviewSidenav peer={this.state.peer} />
           </Drawer>
         </Hidden>
         <Hidden smDown>
@@ -148,22 +102,19 @@ class assigmentsReview extends React.Component {
               paper: classes.drawerPaper
             }}
           >
-            <assigmentsReviewSidenav 
-              assigments={this.state.assigments} 
-              updatedSelectedCard={this.updatedSelectedCard} 
-            />
+            <PeerReviewSidenav peer={this.state.peer} />
           </Drawer>
         </Hidden>
         <main className={classes.content}>
-          <assigmentsReviewDetails selectedCard={this.state.selectedCard}/>
+          <PeerReviewDetails />
         </main>
       </div>
     );
-  }x  
+  }
 }
 
-assigmentsReview.propTypes = {
+Peereview.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(assigmentsReview);
+export default withStyles(styles)(Peereview);
