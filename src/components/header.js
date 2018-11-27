9d949e4c2f5c081268c0580bebc8189dfbd9bda5
Amar Kumar Sinha <amar17@navgurukul.org>
2018-11-27 23:54:02 +0530

    change the name from peer-review to assignment-review

--- src/components/header.js
+++ src/components/header.js
@@ -39,7 +39,7 @@ function Header(props) {
 						</a>
 					</Typography>
 					<IconButton className={classes.menuButton} color="inherit" aria-label="Review Assignments">
-						<a href='/peer-review'>
+						<a href='/assignment-review'>
 							<img src='/static/icons/assignment-logo.png' height='45' />
 						</a>
 					</IconButton>
