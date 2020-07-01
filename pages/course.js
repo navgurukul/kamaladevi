989f559f4e29ad5fb79dba6f586f1ecc917ee744
Abhishek Gupta <a@navgurukul.org>
2020-07-02 01:35:09 +0530

    ok

--- pages/course.js
+++ pages/course.js
@@ -65,7 +65,7 @@ function getQueryVariable(path, variable)
 		console.log(vars);
 		for (var i=0;i<vars.length;i++) {
 			var pair = vars[i].split("=");
-			if(pair[0] == variable) { return pair[1]; }
+			if(pair[0] == variable) { return decodeURIComponent(pair[1]); }
 		}
 		return(false);
 	}
